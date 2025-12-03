3d6b6be5c0a0fadd3aaae96de772ebf3bfa405f1
Merari Cisneros <meraridanielcinseros@gmail.com>
2025-12-02 21:58:26 -0800

    defined function to collect video ids in playlist

--- video_stats.py
+++ video_stats.py
@@ -8,6 +8,8 @@ load_dotenv(dotenv_path="./.env")
 YT_API_KEY = os.getenv("YT_API_KEY")
 part = 'ContentDetails'
 forHandle = 'MrBeast'
+maxResults = 50
+
 
 def get_playlist_id():
 
@@ -34,6 +36,45 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
 
+ 
+
+def get_video_ids(playlistId):
+    video_ids = []
+    pageToken =  None
+    base_url =  f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={maxResults}&playlistId={playlistId}&key={YT_API_KEY}'
+
+    try:
+        while True:
+
+            url = base_url
+
+            if pageToken:
+                url += f'&pageToken={pageToken}'
+            
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+            
+            pageToken = data.get('nextPageToken')
+
+            if not pageToken:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+
+
+
+
 if __name__ == "__main__":
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    get_video_ids(playlistId)
 
