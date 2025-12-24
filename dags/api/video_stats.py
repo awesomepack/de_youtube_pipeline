8be23a9e1ef04d9c762e8271bce5df55ce545443
Merari Cisneros <meraridanielcinseros@gmail.com>
2025-12-24 13:49:38 -0800

    modified url in get_playlist_id to make the DAG succeed

--- dags/api/video_stats.py
+++ dags/api/video_stats.py
@@ -2,80 +2,75 @@ import requests
 import json
 from datetime import date
 
+# import os
+# from dotenv import load_dotenv
+# load_dotenv(dotenv_path="./.env")
+
 from airflow.decorators import task
 from airflow.models import Variable
 
-#Access .env variables
+
 API_KEY = Variable.get("API_KEY")
-part = 'ContentDetails'
-CHANNEL_HANDLE = Variable.get('CHANNEL_HANDLE')
+CHANNEL_HANDLE = Variable.get("CHANNEL_HANDLE")
 maxResults = 50
+
 
 @task
 def get_playlist_id():
-    '''
-    Docstring for get_playlist_id
-    (str) , (str) --> (str)
-    Requests playlist id from youtube API V3 using supplied part and foHandle and returns the matching playlist id
-    '''
 
     try:
-        
-        #Build the url for the request
-     
 
-        url = f'https://youtube.googleapis.com/youtube/v3/channels?part={part}&CHANNEL_HANDLE={CHANNEL_HANDLE}&key={API_KEY}'
-
+        url = f"https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forHandle={CHANNEL_HANDLE}&key={API_KEY}"
 
         response = requests.get(url)
 
-        #Raises HTTP error if code begins with 4 or 5
         response.raise_for_status()
 
         data = response.json()
 
-        channel_items = data['items'][0]
-        channel_playlistId = channel_items['contentDetails']['relatedPlaylists']['uploads']
+        # print(json.dumps(data,indent=4))
 
-        
+        channel_items = data["items"][0]
+
+        channel_playlistId = channel_items["contentDetails"]["relatedPlaylists"][
+            "uploads"
+        ]
+
         return channel_playlistId
-    
+
     except requests.exceptions.RequestException as e:
         raise e
 
- 
+
 @task
 def get_video_ids(playlistId):
-    '''
-    Docstring for get_video_ids
-    (str) --> (list)
-    Loops through response from youtube api v3 50 results at at a time
-    
-    :param playlistId: string produced by get_playlist_id
-    '''
+
     video_ids = []
-    pageToken =  None
-    base_url =  f'https://youtube.googleapis.com/youtube/v3/playlistItems?part=ContentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}'
+
+    pageToken = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&maxResults={maxResults}&playlistId={playlistId}&key={API_KEY}"
 
     try:
+
         while True:
 
             url = base_url
 
             if pageToken:
-                url += f'&pageToken={pageToken}'
-            
+                url += f"&pageToken={pageToken}"
+
             response = requests.get(url)
 
             response.raise_for_status()
 
             data = response.json()
 
-            for item in data.get('items',[]):
-                video_id = item['contentDetails']['videoId']
+            for item in data.get("items", []):
+                video_id = item["contentDetails"]["videoId"]
                 video_ids.append(video_id)
-            
-            pageToken = data.get('nextPageToken')
+
+            pageToken = data.get("nextPageToken")
 
             if not pageToken:
                 break
@@ -88,37 +83,18 @@ def get_video_ids(playlistId):
 
 @task
 def extract_video_data(video_ids):
-    '''
-    Docstring for extract_video_data
-    (List) --> (List of Dictionaries)
-    
-    :param video_ids: (List) of video_ids from get_video_ids
-    '''
 
     extracted_data = []
 
-
-    #Helper function to create batches of video ids
-    def batch_list(video_id_list , batch_size):
-        '''
-        Docstring for batch_list
-        (List) -- > (List)
-        Generates a list of videoids according to maxResults
-        
-        :param video_id_list: (List) Video ids from  get_video_ids
-        :param batch_size: The desired size of the generated list
-        '''
-
-        for video_id in range(0 , len(video_id_list) , batch_size):
-
-            yield video_id_list[video_id: video_id + batch_size]
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0, len(video_id_lst), batch_size):
+            yield video_id_lst[video_id : video_id + batch_size]
 
     try:
-        #Loop through each batch formed from batch list 
-        for batch in batch_list(video_ids , maxResults):
+        for batch in batch_list(video_ids, maxResults):
             video_ids_str = ",".join(batch)
 
-            url = f'https://youtube.googleapis.com/youtube/v3/videos?part=ContentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}'
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_ids_str}&key={API_KEY}"
 
             response = requests.get(url)
 
@@ -126,40 +102,36 @@ def extract_video_data(video_ids):
 
             data = response.json()
 
-            #Loop through response from batch of video ids
-            for item in data.get('items' , []):
-                video_id = item['id']
-                snippet = item['snippet']
-                contentDetails = item['contentDetails']
-                statistics = item['statistics']
-
-                #Store individual video data\
+            for item in data.get("items", []):
+                video_id = item["id"]
+                snippet = item["snippet"]
+                contentDetails = item["contentDetails"]
+                statistics = item["statistics"]
 
                 video_data = {
-                    'video_id': video_id ,
-                    'title': snippet['title'],
-                    'publishedAt': snippet['publishedAt'] ,
-                    'duration': contentDetails['duration'],
-                    'viewCount': statistics.get('viewCount' , None),
-                    'likeCount': statistics.get('likeCount' , None),
-                    'commentCount': statistics.get('commentCount' , None)
-                    }
-                
+                    "video_id": video_id,
+                    "title": snippet["title"],
+                    "publishedAt": snippet["publishedAt"],
+                    "duration": contentDetails["duration"],
+                    "viewCount": statistics.get("viewCount", None),
+                    "likeCount": statistics.get("likeCount", None),
+                    "commentCount": statistics.get("commentCount", None),
+                }
+
                 extracted_data.append(video_data)
-        
+
         return extracted_data
 
     except requests.exceptions.RequestException as e:
         raise e
-    
+
+
 @task
 def save_to_json(extracted_data):
-    file_path = f'./data/YT_data_{date.today()}.json'
+    file_path = f"./data/YT_data_{date.today()}.json"
 
-    with open(file_path , 'w' , encoding='utf-8') as json_outfile:
-        json.dump(extracted_data , json_outfile , indent = 4 , ensure_ascii= False)
-
-
+    with open(file_path, "w", encoding="utf-8") as json_outfile:
+        json.dump(extracted_data, json_outfile, indent=4, ensure_ascii=False)
 
 
 if __name__ == "__main__":
@@ -167,4 +139,3 @@ if __name__ == "__main__":
     video_ids = get_video_ids(playlistId)
     video_data = extract_video_data(video_ids)
     save_to_json(video_data)
-  
