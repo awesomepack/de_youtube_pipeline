fd772d50a06f2fb8ddb2821050f45f43adabb9de
Merari Cisneros <meraridanielcisneros@gmail.com>
2026-01-21 20:01:35 -0800

    Created functions to insert | Update | Delete video data

--- dags/datawarehouse/data_modification.py
+++ dags/datawarehouse/data_modification.py
@@ -2,6 +2,8 @@ import logging
 logger = logging.getLogger(__name__)
 table = "yt_api"
 
+# Insert rows from most recent JSON file
+
 def insert_rows(cur , conn , schema , row):
 
     try:
@@ -39,4 +41,65 @@ def insert_rows(cur , conn , schema , row):
         logger.error(f"Error inserting row with Video_ID: {row[video_id]}")
         raise e
     
-    
+    # Update existing rows
+
+    def update_rows(cur , conn , schema , row):
+        try:
+            # Staging
+            if schema == 'staging':
+                video_id = 'video_id'
+                upload_date = 'publishedAt'
+                video_title = 'title'
+                video_views = 'viewCount'
+                likes_count = 'likesCount'
+                comments_count = 'commentCount'
+
+            # Core
+            else:
+                video_id = 'Video_ID'
+                upload_date = 'Upload_Date'
+                video_title = 'Video_Title'
+                video_views = 'Video_Views'
+                likes_count = 'Likes_Count'
+                comments_count = 'Comments_Count'
+
+            cur.execute(
+                f"""
+                UPDATE {schema}.{table}
+                SET "Video_Title" = %({video_title})s,
+                    "Video_Views" = %({video_views})s,
+                    "Likes_Count" = %({likes_count})s,
+                    "Comments_Count" = %({comments_count})s
+                WHERE "Video_ID" = %({video_id})s AND "Upload_Date" = %({upload_date})s;
+                """ , row
+            )
+
+            conn.commit()
+
+            logger.info(f"Updated row with Video_ID: {row[video_id]}")
+
+        except Exception as e:
+            logger.error(f"Error updating row with Video_ID: {row[video_id]}")
+            raise e
+        
+
+        # Delete videos that have been removed from the channel
+        def delet_rows(cur , conn , schema , ids_to_del):
+            try:
+                ids_to_delete = f"""({' , '.join(f"'{id}'" for id in ids_to_delete)})"""
+
+                cur.execute(
+                    f"""
+                    DELETE FROM {schema}.{table}
+                    WHERE "Video_ID" IN {ids_to_delete}
+                    """
+                )
+
+                conn.commit()
+                logger.info(f"Error deleting rows with Video_IDs: {ids_to_delete}")
+
+            except Exception as e:
+                logger.error(f"Error deleting rows with Video_IDs: {ids_to_delete} - {e}")
+                raise e
+            
+
